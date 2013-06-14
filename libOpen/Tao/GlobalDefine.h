6ae109671a0994fa3e2e061860f2804f7de7a6e1
Tao Yunfei <traintrackcn@gmail.com>
2013-06-14 15:48:16 +0800

    update podspec

--- libOpen/Tao/GlobalDefine.h
+++ libOpen/Tao/GlobalDefine.h
@@ -1,29 +1,34 @@
 
-#import "LoggerClient.h"
+//#import "LoggerClient.h"
 
 
 #ifdef DEBUG
-    #define LOG_DEBUG(__format,__args...) LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"debug", 0, @"%@",[NSString stringWithFormat:__format, ##__args])
+//    #define LOG_DEBUG(__format,__args...) LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"debug", 0, @"%@",[NSString stringWithFormat:__format, ##__args])
+
+    #define LOG_DEBUG(__format,__args...) NSLog(@"%s:%d %@",__FUNCTION__, __LINE__,[NSString stringWithFormat:__format, ##__args]);
 #else
     #define LOG_DEBUG(__format, __args...)  
 #endif
 
 #ifdef DEBUG
-    #define LOG_INFO(__format,__args...) LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"info", 0, @"%@",[NSString stringWithFormat:__format, ##__args])
+//    #define LOG_INFO(__format,__args...) LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"info", 0, @"%@",[NSString stringWithFormat:__format, ##__args])
+#define LOG_INFO(__format,__args...) NSLog(@"%@",[NSString stringWithFormat:__format, ##__args])
 #else
     #define LOG_INFO(__format, __args...)
 #endif
 
 
 #ifdef DEBUG
-    #define LOG_ERROR(__format,__args...) LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"error", 0, @"%@",[NSString stringWithFormat:__format, ##__args])
+//    #define LOG_ERROR(__format,__args...) LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"error", 0, @"%@",[NSString stringWithFormat:__format, ##__args])
+#define LOG_ERROR(__format,__args...) NSLog(@"%@",[NSString stringWithFormat:__format, ##__args])
 #else
     #define LOG_ERROR(__format, __args...)
 #endif
 
 //#define LOG_TEST(__format,__args...) LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"test", 0, @"%@",[NSString stringWithFormat:__format, ##__args])
 
-#define ccp(__X__,__Y__) CGPointMake(__X__,__Y__)
+#define CCP(__X__,__Y__) CGPointMake(__X__,__Y__)
+#define RGBA(r, g, b, a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
 #define PTM_RATIO 32.0
 #define RANDOM_INT arc4random()%999999999
 
