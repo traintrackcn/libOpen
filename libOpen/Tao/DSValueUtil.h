50d4c9c76bc7a0dc4e859bf437b458cdcf794720
Tao Yunfei <yunfei.tao@2vive.com>
2014-05-16 09:46:55 +0800

    added DSValueUtil

---
+++ libOpen/Tao/DSValueUtil.h
@@ -0,0 +1,32 @@
+//
+//  DSValue.h
+//  og
+//
+//  Created by traintrackcn on 13-10-5.
+//  Copyright (c) 2013 2ViVe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DSValueUtil : NSObject
+
+
++ (BOOL)isAvailable:(id)value;
++ (BOOL)isNotAvailable:(id)value;
+
++ (NSString *)toString:(id)value;
++ (CGFloat)toFloat:(id)value;
++ (NSInteger)toInteger:(id)value;
+
++ (BOOL)isNil:(id)value;
++ (BOOL)isNotNil:(id)value;
++ (BOOL)isNull:(id)value;
++ (BOOL)isNotNull:(id)value;
+
++ (BOOL)isEmptyString:(NSString *)value;
++ (BOOL)isNotEmptyString:(NSString *)value;
+
++ (NSArray *)convertRawArray:(NSArray *)rawArray modelClass:(Class)modelClass;
++ (id)itemAtIndex:(NSInteger)index inArray:(NSArray *)array;
+
+@end
